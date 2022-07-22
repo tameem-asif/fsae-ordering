938ee372289057d22f91270e460f736ae24db666
Tameem Asif <tameem.asif02@gmail.com>
2022-07-22 00:34:12 +0000

    Server modified to support HTTPS

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
+// All dependencies
 const express = require('express');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+// Declaring the app
 const app = express();
 
-app.use(express.static('public'))
+// Certificates
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/tameem.dev/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/tameem.dev/fullchain.pem', 'utf8');
 
-app.listen(8080, () => console.log('FSAE app is listening on port 8080.'));
+const credentials = {
+	key: privateKey,
+	cert: certificate
+};
+
+// Middleware
+app.use(express.static('public', { dotfiles: 'allow' } ));
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+// Creating HTTP and HTTPS servers
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
+
+//app.listen(8080, () => {
+//	console.log('FSAE app is listening on port 8080.');
+//});
