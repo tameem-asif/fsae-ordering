4c1a149f9c6768f81a2309c1ed2038864658510a
Tameem Asif <tameem.asif02@gmail.com>
2022-08-19 21:10:03 +0000

    api functionality for getAll has been implemented

--- server.js
+++ server.js
@@ -5,17 +5,20 @@ const http = require('http');
 const https = require('https');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 
 // MongoDB Connection
-mongoose.connect("mongodb+srv://tameem:<password>@cu-fsae.wdkxp.mongodb.net/?retryWrites=true&w=majority");
-const db = mongoose.connection;
-db.on("Error", console.log.bind(console, "connection error"));
-db.once("open", function(callback) {
+ mongoose.connect("mongodb+srv://tameem:2SGyBxdGMLKpJ6UY@cu-fsae.wdkxp.mongodb.net/?retryWrites=true&w=majority");
+const client = mongoose.connection;
+
+client.on("Error", console.log.bind(console, "connection error"));
+client.once("open", function(callback) {
 	console.log("connection succeeded");
 })
 
 // Declaring the app
 const app = express();
+const router = express.Router();
 var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -29,7 +32,22 @@ const credentials = {
 };
 
 // Middleware
-app.use(express.static('public', { dotfiles: 'allow' } ));
+app.use(express.static('public'));
+app.use("/api", router);
+
+// Routes + REST API Functionality
+const orders = client.collection("active-orders");
+
+router.get("/getAll", async (req,res) => {
+	try {
+		const data_all = orders.find();
+		const data_array = await data_all.toArray();
+		res.json(data_array);
+	}
+	catch(error) {
+		res.status(500).json({message: error.message});
+	}
+});
 
 //POST Request
 app.post("/form-handler", urlencodedParser, function(req,res) {
@@ -63,13 +81,14 @@ app.post("/form-handler", urlencodedParser, function(req,res) {
 		"add_info": add_info
 	}
 
-	db.collection("active-orders").insertOne(data, function(err, collection) {
+	client.collection("active-orders").insertOne(data, function(err, collection) {
 		if (err) throw err;
 		console.log("Record inserted successfully");
 	});
 
 	res.redirect("/");
 });
+
 
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
